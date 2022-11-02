f4cc0f41a9a6bbbbc6e8113dceeb56d130160657
ericPrimelles <eric.a.primelles96@gmail.com>
2022-11-02 14:29:33 -0400

    @tf.function

--- MADDPG.py
+++ MADDPG.py
@@ -42,7 +42,9 @@ class MADDPG:
                 'target_a_n' : DDPGActor(self.obs_space[1], self.action_space[1]),
                 'q_n' : DDPGCritic(self.obs_space[0] * self.obs_space[1], self.action_space[0]),
                 'target_q_n' : DDPGCritic(self.obs_space[0] * self.obs_space[1], self.action_space[0]),
-                'loss_fn' : mean_squared_error
+                'loss_fn' : mean_squared_error,
+                'q_opt' : Adam(1e-4),
+                'a_opt' : Adam(1e-5)
             }
             for agnt in range(self.n_agents)
         ]
@@ -55,20 +57,13 @@ class MADDPG:
         
     def noise_reset(self):
         self.x_prev = np.zeros((self.n_agents, 2))
-        
-    def updateTarget(self):
-        
+    
+    @tf.function    
+    def updateTarget(self, target_weights, weights, i):
+        for (a, b) in zip(target_weights, weights):
+            a.assign(b * self.tau + a * (1 - self.tau))
             
-        for i in self.agents:
-            a_w = np.array(i['a_n'].weights, dtype=object)
-            q_w = np.array(i['q_n'].weights, dtype=object)
-            a_t_w = np.array(i['target_a_n'].weights, dtype=object)
-            q_t_w = np.array(i['target_q_n'].weights, dtype=object)
-            
-            
-            i['target_a_n'].set_weights(a_t_w * self.tau + (1 - self.tau) * a_w)
-            i['target_q_n'].set_weights(q_t_w * self.tau + (1 - self.tau) * q_w) 
-
+       
     def save(self):
         for i in self.agents:
             _id = i['id']
@@ -93,27 +88,22 @@ class MADDPG:
         
         return a * 1 / norm    
     
-    
+    @tf.function
     def chooseAction(self, s : tf.Tensor, target : bool = False, training : bool = False):
-        if s.ndim == 2:
-            s = s.reshape((1, s.shape[0], s.shape[1]))
             
-        actions = []
-              
-        
-        for i in range(self.n_agents):
-            x = s[:,i, :]
-            #x = x.reshape((self.batch_size, 1, self.obs_space[1]))
+            if s._rank() <= 2:
+                s = tf.expand_dims(s, 0)
             
-            if not target:
             
-                actions.append(self.normalize(self.agents[i]['a_n'](x, training)))    
-            
-            else:
-                actions.append(self.normalize(self.agents[i]['target_a_n'](x, training)))
-        return actions
-    
-    
+            acts = tf.stack([
+               self.agents[i]['a_n'](s[:, i, :], training)
+               for i in range(self.n_agents) 
+            ])
+            '''for i in range(self.n_agents):
+                s = s[:, i, :]
+                act = self.agents[i]['a_n'](s, training)
+                acts[:,i, :].assign(act)'''
+            return acts
     def policy(self, s):
         a = np.squeeze(np.array(self.chooseAction(s)))
         
@@ -151,8 +141,19 @@ class MADDPG:
                     self.rb.store(s, a, r, s_1, done)
                     
                     if self.rb.ready:
-                        self._learn(self.rb.sample())
-                        self.updateTarget()
+                        s_s, a_s, r_s, s_1_s, dones_s = self.rb.sample()
+                        #a_s = a_s.reshape((self.n_agents, 64, 2))
+                        s_s = tf.convert_to_tensor(s_s)
+                        #a_s = tf.convert_to_tensor(a_s)
+                        r_s = tf.convert_to_tensor(r_s)
+                        s_1_s = tf.convert_to_tensor(s_1_s)
+                        dones_s = tf.convert_to_tensor(dones_s)
+                        
+                        for i in range(self.n_agents):
+                            self._learn(s_s, a_s, r_s, s_1_s, i)
+                            self.updateTarget(self.agents[i]['target_q_n'].weights, self.agents[i]['q_n'].weights, i)
+                            self.updateTarget(self.agents[i]['target_a_n'].weights, self.agents[i]['a_n'].weights, i)
+                            
                         
                     s = s_1
                     ts +=1
@@ -175,53 +176,47 @@ class MADDPG:
             dump(rwd, self.path + f'reward_epcohs_{i}.joblib')
                 #print(f'Epoch: {i + 1} / {self.n_epochs} Episode {j + 1} / {self.n_episodes} Reward: {reward / ts}')        
                   
-          
-    def _learn(self, sampledBatch):
-        s, a, r, s_1, dones = sampledBatch
-        a = a.reshape((self.n_agents, 64, 2))
-        s = tf.convert_to_tensor(s)
-        #a = tf.convert_to_tensor(a)
-        r = tf.convert_to_tensor(r)
-        s_1 = tf.convert_to_tensor(s_1)
-        dones = tf.convert_to_tensor(dones)
+    @tf.function    
+    def _learn(self, s, a, r, s_1, i):
         
         
-        for i in range(self.n_agents):
-            s_agnt = s[:, i]
-            a_agnt = a[i]
-            r_agnt = r[:, i]
-            s_1_agnt = s_1[:, i]
-            dones_agnt = dones[i]
-            agnt = self.agents[i]
-            opt = Adam(self.l_r)
-            with tf.GradientTape() as tape:
-            
-                acts = self.chooseAction(s_1, True, True)
-                
-                y = r_agnt + self.gamma * agnt['target_q_n']([flatten(s_1), [acc for acc in acts]])
-                
-                q_value = agnt['q_n']([flatten(s), [acc for acc in a]])
-                q_loss = tf.math.reduce_mean(tf.math.square(y - q_value))   
-                
-            q_grad = tape.gradient(q_loss, agnt['q_n'].trainable_variables)
-            opt.apply_gradients(zip(q_grad, agnt['q_n'].trainable_variables))
-            
+
+        s_agnt = s[:, i]
+        a_agnt = a[:, i]
+        r_agnt = r[:, i]
+        s_1_agnt = s_1[:, i]
+        agnt = self.agents[i]
+        
+        with tf.GradientTape() as tape:
+        
+            acts = self.chooseAction(s_1, True, True)
+
+            y = r_agnt + self.gamma * agnt['target_q_n']([flatten(s_1), tf.squeeze(tf.split(acts, self.n_agents))])
+
+            q_value = agnt['q_n']([flatten(s), tf.squeeze(tf.split(a, self.n_agents))])
+            q_loss = tf.math.reduce_mean(tf.math.square(y - q_value))   
+
+        q_grad = tape.gradient(q_loss, agnt['q_n'].trainable_variables)
+        agnt['q_opt'].apply_gradients(zip(q_grad, agnt['q_n'].trainable_variables))
+        
+        #acts = self.chooseAction(s, training=True)
+
+        with tf.GradientTape(True) as tape:
+
+            #act = agnt['a_n'](s_agnt, training=True)
             acts = self.chooseAction(s, training=True)
-            
-            with tf.GradientTape(True) as tape:
-                
-                act = agnt['a_n'](s_agnt, training=True)
-                q_values = agnt['q_n'](([
-                    flatten(s),
-                    [acc for acc in acts[0:i]],
-                    act,
-                    [acc for acc in acts[i+1:]],
-                ]))
-                loss = -tf.reduce_mean(q_values)
-            a_grad = tape.gradient(loss, agnt['a_n'].trainable_variables)
-            opt.apply_gradients(zip(a_grad, agnt['a_n'].trainable_variables))
-            
-                       
+
+            '''q_values = agnt['q_n'](([
+                flatten(s),
+                [acc for acc in acts[0:i]],
+                act,
+                [acc for acc in acts[i+1:]],
+            ]))'''
+            q_values = agnt['q_n']([flatten(s), tf.squeeze(tf.split(acts, self.n_agents))])
+            loss = -tf.reduce_mean(q_values)
+        a_grad = tape.gradient(loss, agnt['a_n'].trainable_variables)
+        agnt['a_opt'].apply_gradients(zip(a_grad, agnt['a_n'].trainable_variables))
+
     def test(self):
         self.load()
         s = self.env.reset()
@@ -258,10 +253,8 @@ class MADDPG:
         
 if __name__ == '__main__':
     
-     env = DeepNav(3, 0)
+     env = DeepNav(1, 0)
 
 
      p = MADDPG(env)
      p.Train()
-     p.test()
-     p.plot()
